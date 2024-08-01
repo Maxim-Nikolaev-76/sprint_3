a26bc0ef6e1e5249277947d72a7bea4263310e32
Николаев Максим Валерьевич <Nikolaev_MV@local.uralkali.com>
2024-08-01 17:15:39 +0500

    Add save results.

--- game.py
+++ game.py
@@ -48,13 +48,23 @@ def main():
         game.display()
         # После каждого хода надо делать проверку на победу и на ничью.
         if game.check_win(current_player):
-            print(f'Победили {current_player}!')
+            winer = f'Победили {current_player}!\n'
+            print(winer)
+            save_result(winer)
             running = False
         elif game.is_board_full():
-            print('Ничья!')
+            draw = 'Ничья!\n'
+            print(draw)
+            save_result(draw)
             running = False
 
         current_player = 'O' if current_player == 'X' else 'X'
+
+
+def save_result(result):
+    file = open('results.txt', 'a', encoding='utf-8')
+    file.write(result)
+    file.close()
 
 
 if __name__ == '__main__':
