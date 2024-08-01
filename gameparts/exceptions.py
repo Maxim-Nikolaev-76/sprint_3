43b8071df106dd1bb8f961101a1236e02debfbd6
Николаев Максим Валерьевич <Nikolaev_MV@local.uralkali.com>
2024-08-01 16:51:22 +0500

    Game release .0

---
+++ gameparts/exceptions.py
@@ -0,0 +1,17 @@
+# gameparts/exceptions.py
+"""
+Классы для обработки исключений.
+"""
+
+
+class FieldIndexError(IndexError):
+
+    def __str__(self):
+        return 'Введено значение за границами игрового поля'
+
+
+# Вот оно - новое исключение, унаследованное от базового класса Exception.
+class CellOccupiedError(Exception):
+
+    def __str__(self):
+        return 'Попытка изменить занятую ячейку'
