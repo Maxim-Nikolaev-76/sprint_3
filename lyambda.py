9f5bdff9fa51f7cb5a243fddddc0bc1bc555ba6f
Николаев Максим Валерьевич <Nikolaev_MV@local.uralkali.com>
2024-08-02 13:39:28 +0500

    Add  new file lambda

---
+++ lyambda.py
@@ -0,0 +1,16 @@
+people = ['Антон', 'Соня', 'Коля', 'Женя', 'Тоня', 'Стёпа']
+
+
+def say_to_all(func, sequence):
+    for item in sequence:
+        func(item)
+
+
+# Этот вызов для каждого имени из списка должен напечатать
+# строчку Привет, <имя>!
+say_to_all(lambda name: print(f'Здравствуй, {name}!') if name[0] == 'С'
+           else print(f'Привет, {name}!'), people)
+# Этот вызов для каждого имени из списка должен напечатать
+# строчку До завтра, <имя>!
+# lambda <переменная>: <функция>, <данные>
+say_to_all(lambda name: print(f'До завтра, {name}!'), people)
